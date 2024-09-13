3ea0f6b50fa395e0940f98c33d6a76835e7f3920
Mauricio <mauriciohillesheim15@gmail.com>
2024-09-13 16:28:13 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'; // Importação do componente principal
+import './index.css'; // Importação dos estilos globais
+import { BrowserRouter } from 'react-router-dom'; // Importação do BrowserRouter para habilitar o roteamento
+ 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
